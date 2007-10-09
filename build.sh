c1be2d0489f6720c3a19bc6427a397f875ee8d1e
Sean Hodges <seanhodges@bluebottle.com>
2007-10-09 23:51:43 +0100

    Removed StringEnum class from build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 g++ -g -o application.bin \
 	`pkg-config --cflags --libs gtk+-2.0,libxml-2.0,firefox-gtkmozembed` \
-	StringEnum.cpp ConfigContainer.cpp ConfigReader.cpp AppWindow.cpp GeckoEmbed.cpp AppMain.cpp
+	ConfigContainer.cpp ConfigReader.cpp AppWindow.cpp GeckoEmbed.cpp AppMain.cpp
