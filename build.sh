7ae59e53e8ff0ab4e6aa0ed1f61e54f240d9f0d6
Sean Hodges <seanhodges@bluebottle.com>
2007-10-09 23:49:36 +0100

    Cleared up build.sh (removed commented targets)

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#g++ geckotest.c -o geckotest `pkg-config --cflags --libs gtk+-2.0,firefox-gtkmozembed,firefox-xpcom,firefox-js,firefox-nspr,firefox-nss,firefox-plugin` -lgtkembedmoz -lmozjs -lxpcom
-#g++ AppMain.cpp -o application.bin `pkg-config --cflags --libs gtk+-2.0,firefox-gtkmozembed,firefox-xpcom,firefox-js,firefox-nspr,firefox-nss,firefox-plugin` -lgtkembedmoz -lmozjs -lxpcom
-
-#g++ -g -o application.bin \
-#	`pkg-config --cflags --libs gtk+-2.0,libxml-2.0,firefox-gtkmozembed,firefox-xpcom,firefox-js,firefox-nspr,firefox-nss,firefox-plugin` \
-#	-lgtkembedmoz -lmozjs -lxpcom \
-#	ConfigReader.cpp AppWindow.cpp GeckoEmbed.cpp AppMain.cpp
-
 g++ -g -o application.bin \
 	`pkg-config --cflags --libs gtk+-2.0,libxml-2.0,firefox-gtkmozembed` \
-	ConfigContainer.cpp ConfigReader.cpp AppWindow.cpp GeckoEmbed.cpp AppMain.cpp
+	StringEnum.cpp ConfigContainer.cpp ConfigReader.cpp AppWindow.cpp GeckoEmbed.cpp AppMain.cpp
