10bbcc08c1bebf79188d4f083764c09009185491
Sean Hodges <seanhodges@bluebottle.com>
2007-10-11 22:39:40 +0100

    Added a comment to build.sh, mainly testing commiting in this branch and merging it to trunk

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Build the Medes binary
+
 g++ -g -o medes \
 	`pkg-config --cflags --libs gtk+-2.0,libxml-2.0,firefox-gtkmozembed` \
 	ConfigContainer.cpp ConfigReader.cpp AppWindow.cpp GeckoEmbed.cpp AppMain.cpp
