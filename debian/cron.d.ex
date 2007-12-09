546c5806e425c4d61e46f74682fa6232cff6c758
Sean Hodges <seanhodges@bluebottle.com>
2007-12-09 14:37:25 +0000

    Removed the unused debian/ files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the medes package
-#
-0 4	* * *	root	medes_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(medes):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="medes" command="/usr/bin/medes"
